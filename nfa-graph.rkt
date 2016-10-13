cad7771a0f7263a388b15e4c4e2215dfa864a8f2
Sebastian Kimberk <skimberk1@gmail.com>
2016-10-12 20:43:37 -0400

    Updated graph NFA to use 'shared' and 'box'. It should work now.

--- nfa-graph.rkt
+++ nfa-graph.rkt
@@ -1,20 +1,25 @@
 #lang racket
 
-;;; A State is a (make-state ListOfTransitions)
-(define-struct state (transitions))
-
-;;; A Transition is a (make-transition Character State)
-(define-struct transition (char to-state))
-
-;;; A StateIndex is an Integer.
-
-;;; An NFA is a State.
-
 ;;; A ListOfTransitions is one of:
 ;;; - empty
 ;;; - (cons Transition ListOfTransitions)
 
-(define concat-nfa (letrec ([f (make-state (list (make-transition '() f)))]
-                            [b (make-state (list (make-transition #\B f)))]
-                            [a (make-state (list (make-transition #\A b)))])
-                     a))
+;;; An Input is either 'empty or Character
+
+;;; A State is a (make-state ListOfTransitions)
+(define-struct state (transitions))
+
+;;; A Transition is a (make-transition Input (box State))
+(define-struct transition (char to-state))
+
+;;; An NFA is a (make-nfa (box State) (box State))
+(define-struct nfa (start final))
+
+;;; Test NFA matching regular expression "AB"
+(define test-nfa (shared ([f (box (make-state (list (make-transition 'empty
+                                                                     f))))]
+                          [b (box (make-state (list (make-transition #\B
+                                                                     f))))]
+                          [a (box (make-state (list (make-transition #\A
+                                                                     b))))])
+                   (make-nfa a f)))
